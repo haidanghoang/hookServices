5bf49409fb1368a9c3ad63ad7cb62711cf7df2a0
Hai Dang Hoang <haidanghoang@billion.com>
2020-10-07 08:58:57 +0700

    update index

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Hook</h1>');
+})
+
 app.post('/hooks', (req, res) => {
-  console.log(req.body);
+  res.json({
+    message: 'Received.',
+    ...req.body,
+  });
 });
+
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
