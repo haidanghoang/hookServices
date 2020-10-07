44f8ca7634d4a76dcb55c0404fe900fbc0ac696f
Hai Dang Hoang <haidanghoang@billion.com>
2020-10-07 09:21:57 +0700

    update hooks

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let requests = [];
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Hook</h1>');
-})
+  res.send(`
+    <h1>Hello hooks</h1>
+    ${JSON.stringify(requests, null, 2)}
+  `);
+});
 
 app.post('/hooks', (req, res) => {
   res.json({
     response: 'Received.',
     ...req.body,
   });
+  requests.push(req.body);
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
